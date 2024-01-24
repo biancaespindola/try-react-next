11cba723c4e143be82f8051f1f3749e530d52bf6
Bianca Espíndola <32000570+biancaespindola@users.noreply.github.com>
2024-01-24 20:35:24 +0100

    test React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { useState } from "react";
 
 import "./App.css";
-import Heading from "./Heading.";
+
+import AboutMe from "./AboutMe";
+import Heading from "./Heading";
+import Home from "./Home";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [word, setword] = useState("Hi");
 
   return (
     <>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="about-me">About me</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-me" element={<AboutMe />} />
+        </Routes>
+        {/* <Home />
+        <AboutMe /> */}
+      </div>
+
       <Heading word={word} />
       <button onClick={() => setword("Bye")}>Change</button>
     </>
