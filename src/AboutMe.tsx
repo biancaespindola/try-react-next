08d764ac4e0e570c32079d34ddada571838580b4
Bianca Espíndola <32000570+biancaespindola@users.noreply.github.com>
2024-01-26 11:54:01 +0100

    test import images

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -1,7 +1,11 @@
+import santa from "./assets/santa-claus.png";
+
 function AboutMe() {
   return (
     <div>
       <h1>About me</h1>
+      <p>I'm Santa Claus</p>
+      <img src={santa} alt="santa" />
     </div>
   );
 }
