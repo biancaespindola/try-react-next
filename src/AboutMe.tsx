110ed504396940e75864bc4ed088b553b9022eac
Bianca Espíndola <32000570+biancaespindola@users.noreply.github.com>
2024-01-26 12:03:24 +0100

    second way to import a img

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -1,11 +1,14 @@
 import santa from "./assets/santa-claus.png";
 
 function AboutMe() {
+  const santa2 =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuV-Dgl9pPCSbKlKXlyI2mwEzxiEsUdhoH2Q&usqp=CAU";
   return (
     <div>
       <h1>About me</h1>
       <p>I'm Santa Claus</p>
       <img src={santa} alt="santa" />
+      <img src={santa2} alt="santa" />
     </div>
   );
 }
